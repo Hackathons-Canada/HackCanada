7832e937101646cf52d8c2e9428180cae5e594fd
Jamerson <jamescao800@gmail.com>
2024-06-27 13:18:47 -0400

    made the start of the river section - james

--- HackCanada/src/components/About.tsx
+++ HackCanada/src/components/About.tsx
@@ -1,10 +1,20 @@
 function About() {
   return (
-    <main className="py-10 px-3.5 w-full h-[200vh] bg-cover bg-top bg-[url('.\assets\aboutbg.png')]">
-      <h1 className="pl-[1.3em] pt-[.3em] text-left text-3 text-[9em] font-extrabold text-[#E6F6FF]">
-        Hack Canada <br />
-        2025
-      </h1>
+    <main className="flex items-center justify-center h-screen py-10 px-3.5 w-full bg-cover bg-top bg-[url('.\assets\aboutbg.png')]">
+      <div className="container text-left text-[#E6F6FF] max-w-5xl">
+        <h2 className="mt-4 text-4xl font-extrabold">What is HackCanada?</h2>
+        <h3 className="text-3xl mt-4">
+          This September, join 1,000+ hackers from around the world for a
+          hackathon like no other. Discover a community of like-minded hackers,
+          connect with world-class mentors, and build because you love to build.
+          At Hack the North 11, you're in for a great time with engaging
+          workshops, fun activities, and the chance to network with the most
+          exciting companies in tech! Not from Waterloo? We cover food, travel
+          expenses, and lodging so you can focus on turning your dreams into
+          reality.
+        </h3>
+      </div>
+      <div></div>
     </main>
   );
 }
