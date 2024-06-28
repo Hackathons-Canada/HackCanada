a935e3fdf8710a907326c56bc7ab5fd9ef38e03b
araf821 <araf.ahmed200@gmail.com>
2024-06-27 22:41:23 -0400

    feat: redesigned about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,41 +1,42 @@
+import { aboutSectionMilestones } from "../../lib/constants";
+
 function About() {
   return (
-    <section className="w-full px-3.5 py-10">
-      <div className="container mx-auto max-w-5xl text-left text-[#E6F6FF]">
-        <h3 className="mt-4 text-4xl font-extrabold">What is HackCanada?</h3>
-        <h4 className="mt-4 text-3xl">
-          This September, join 1,000+ hackers from around the world for a
-          hackathon like no other. Discover a community of like-minded hackers,
-          connect with world-class mentors, and build because you love to build.
-          At Hack the North 11, you're in for a great time with engaging
-          workshops, fun activities, and the chance to network with the most
-          exciting companies in tech! Not from Waterloo? We cover food, travel
-          expenses, and lodging so you can focus on turning your dreams into
-          reality.
-        </h4>
-      </div>
-      <div className="flex flex-row">
-        <div>
-          <h2>12</h2>
-          <h3>Fun Events</h3>
-        </div>
-        <div>
-          <h2>12</h2>
-          <h3>Fun Events</h3>
-        </div>
-        <div>
-          <h2>12</h2>
-          <h3>Fun Events</h3>
-        </div>
-        <div>
-          <h2>12</h2>
-          <h3>Fun Events</h3>
-        </div>
-        <div>
-          <h2>12</h2>
-          <h3>Fun Events</h3>
+    <section className="px-6 py-20 sm:px-12 md:py-28 lg:py-36">
+      <div className="mx-auto mb-16 max-w-screen-md md:mb-24">
+        <h3 className="font-alfa mb-6 text-xl tracking-wider text-white md:mb-8 md:text-2xl lg:text-3xl">
+          What is HackCanada?
+        </h3>
+        <div className="space-y-2.5 font-medium text-white/75 md:space-y-4 md:text-lg">
+          <p>
+            This September, join 1,000+ hackers from around the world for a
+            hackathon like no other. Discover a community of like-minded
+            hackers, connect with world-class mentors, and build because you
+            love to build. At Hack the North 11, you're in for a great time with
+            engaging workshops, fun activities, and the chance to network with
+            the most exciting companies in tech!
+          </p>
+          <p>
+            Not from Waterloo? We cover food, travel expenses, and lodging so
+            you can focus on turning your dreams into reality.
+          </p>
         </div>
       </div>
+      <ul className="mx-auto grid max-w-screen-lg grid-cols-2 gap-8 sm:gap-16 md:grid-cols-4 lg:gap-8">
+        {aboutSectionMilestones.map(({ heading, subheading }) => (
+          <li
+            key={heading}
+            className="flex flex-col items-center justify-center"
+          >
+            <p className="font-alfa text-center text-4xl text-white lg:text-5xl">
+              {heading}
+            </p>
+            <p className="text-center text-lg font-medium text-white/75 lg:text-xl">
+              {subheading}
+            </p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -15,13 +15,13 @@ function Hero() {
       />
       <div className="absolute inset-x-0 -bottom-40 top-0 bg-gradient-to-b from-transparent via-transparent via-[80%] to-[#c2cc59]"></div>
 
-      <div className="relative m-auto min-h-[80vh] w-fit space-y-4 px-8 py-20 md:py-28 md:text-center lg:py-36">
-        <h1 className="font-alfa text-7xl tracking-wide text-lime-950 drop-shadow-sm lg:text-8xl">
+      <div className="relative m-auto min-h-[80vh] w-fit space-y-4 px-8 py-20 md:py-28 lg:py-36">
+        <p className="font-alfa text-7xl tracking-wide text-lime-950 drop-shadow-sm md:text-center lg:text-8xl">
           Hack Canada 2025
-        </h1>
-        <h2 className="mx-auto w-fit bg-lime-900 px-2 py-1 text-2xl font-medium text-zinc-100 drop-shadow-xl">
+        </p>
+        <p className="w-fit rounded-md px-2 py-1 text-lg font-semibold text-lime-950 sm:text-xl md:mx-auto md:bg-lime-900 md:text-center md:text-lime-100 lg:text-2xl">
           Coming Soon • In-person event • Unofficial MLH Member
-        </h2>
+        </p>
       </div>
     </section>
   );
