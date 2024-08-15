9a8b8cdefde90b20422eb6031cd41a648bad2d36
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2024-08-15 19:23:05 -0400

    feat: add all sections Skeleton

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,16 @@
-export const siteConfig = {
+export const heroContent = {
   title: "HackCanada",
+  year: "2025",
+  tagline: "Innovate. Connect. Transform.",
+};
+
+export const aboutContent = {
+  title: "About us",
   description:
     "The largest hackathon in Canada. With over 3000+ hackers and 300+ sponsors, we aim to bring together the best hackers from around the world to create projects that will push forward the world of technology!",
-  // More SEO stuff to add as we go
+};
+
+export const sponsorContent = {
+  sponsors: [{ sponsorName: "", sponsorImage: "", sponsorWebsite: "" }],
+  tagline: "more to come",
 };
