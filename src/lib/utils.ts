cf983136b6f5f4843b0f0c99c59831a816a95c9b
Jason Cameron <git@jasoncameron.dev>
2024-09-13 00:14:05 -0400

    Fix astro check errors (and remove unused cn utils wait())
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Merges multiple class names into a single class string.
- *
- * @param {...ClassValue} inputs - Variable number of class values to merge.
- * @return {string} A single class string containing all input classes.
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-/**
- * This function is for testing loading states.
- * Waits for the specified number of milliseconds.
- *
- * @param {number} ms - The number of milliseconds to wait.
- * @return {Promise<void>} A promise that resolves after the specified time.
- */
-export function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
