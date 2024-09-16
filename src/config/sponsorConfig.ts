bbe5618aa200dd256387c880ffaeaf488772dbc0
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-09-16 15:03:19 -0400

    basic css setup for the 3d component

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -8,7 +8,7 @@ export const sponsorConfig = {
     text: "BECOME A SPONSOR",
     buttonText: "Get in Touch",
     buttonClassName:
-      "bg-yellow-100 text-slate-700 hover:bg-yellow-200 focus:ring-2 focus:ring-yellow-500 rounded-md py-2 px-4 transition duration-200",
+      "bg-yellow-100 text-slate-700 hover:bg-yellow-200 focus:ring-2 focus:ring-yellow-500 rounded-md py-2 px-4 transition duration-200 ",
     email: "sponsor@hackcanada.org", //change this or wtv
   },
   sponsors: [
