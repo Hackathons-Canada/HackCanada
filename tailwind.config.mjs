6405adf96f88971acc0644fdf0aab8c937de8f20
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-09-28 16:25:39 -0400

    clean up unused css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,6 @@ import tailwindTypography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,}"],
   prefix: "",
   theme: {
@@ -16,41 +15,6 @@ export default {
       },
     },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -74,32 +38,18 @@ export default {
           "0%, 100%": { transform: "translateY(0)" },
           "50%": { transform: "translateY(-10px)" },
         },
-        fadeIn: {
-          "0%": { opacity: 0, scale: 0 },
-          "100%": { opacity: 1, scale: 1 },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         rotateSlow: "rotateSlow 20s linear infinite",
         float: "float 6s ease-in-out infinite",
-        fadeIn: "fadeIn 0.5s ease-in-out",
-      },
-      textShadow: {
-        "3d": "0 1px 0 #e0e0e0, 0 2px 0 #d9d9d9, 0 3px 0 #ccc, 0 4px 0 #c9c9c9, 0 5px 0 #bbb, 0 6px 1px rgba(0,0,0,.05), 0 0 3px rgba(0,0,0,.05), 0 1px 2px rgba(0,0,0,.15), 0 2px 3px rgba(0,0,0,.1), 0 3px 6px rgba(0,0,0,.12), 0 5px 5px rgba(0,0,0,.1), 0 10px 10px rgba(0,0,0,.08)",
       },
     },
   },
   plugins: [
-    plugin(({ addVariant, addUtilities }) => {
+    plugin(({ addVariant }) => {
       addVariant("hocus", ["&:hover", "&:focus"]);
-      addUtilities({
-        ".text-3d": {
-          "text-shadow":
-            "0 1px 0 #e0e0e0, 0 2px 0 #d9d9d9, 0 3px 0 #ccc, 0 4px 0 #c9c9c9, 0 5px 0 #bbb, 0 6px 1px rgba(0,0,0,.05), 0 0 3px rgba(0,0,0,.05), 0 1px 2px rgba(0,0,0,.15), 0 2px 3px rgba(0,0,0,.1), 0 3px 6px rgba(0,0,0,.12), 0 5px 5px rgba(0,0,0,.1), 0 10px 10px rgba(0,0,0,.08)",
-        },
-      });
     }),
     animatePlugin,
     tailwindTypography,
