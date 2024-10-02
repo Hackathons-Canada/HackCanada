1410c79f30d4d3b9c874b192f5f4a70716dcc34a
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-02 03:39:14 -0400

    update url

--- src/config/site.ts
+++ src/config/site.ts
@@ -14,7 +14,7 @@ export const siteConfig = {
   title: "HackCanada",
   description:
     "The largest hackathon in Canada. With over 3000+ hackers and 300+ sponsors, we aim to bring together the best hackers from around the world to create projects that will push forward the world of technology!",
-  url: "https://hackcanada.org",
+  url: "https://staging.hackcanada.org",
 
   og: {
     imageType: "image/png",
