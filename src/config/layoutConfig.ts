9b3cd2d4f6a8991db55b3e4c965c632bb8d10065
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-02 00:18:51 -0400

    break common parts

---
+++ src/config/layoutConfig.ts
@@ -0,0 +1,42 @@
+import { Discord, Gmail, Instagram, Linkedin } from "@/lib/icon-library";
+
+export interface SocialLink {
+  href: string;
+  // eslint-disable-next-line
+  icon: (props: { size: number; color: string; className?: string }) => string;
+  label: string;
+}
+
+export const socialLinks: SocialLink[] = [
+  { href: "https://discord.gg/wp42amwcWy", icon: Discord, label: "Discord" },
+  {
+    href: "https://www.instagram.com/hackathoncanada/",
+    icon: Instagram,
+    label: "Instagram",
+  },
+  {
+    href: "https://www.linkedin.com/company/hackathonscanada/",
+    icon: Linkedin,
+    label: "LinkedIn",
+  },
+  { href: "mailto:contact@hackcanada.org", icon: Gmail, label: "Email" },
+];
+
+export const footerLinksRow1 = [
+  { label: "Apply as a Mentor", href: "", disabled: true },
+  { label: "Apply as a Judge", href: "", disabled: true },
+];
+
+export const footerLinksRow2 = [
+  { label: "Terms of Service", href: "" },
+  { label: "Privacy Policy", href: "/privacy" },
+  { label: "Contact Us", href: "mailto:contact@hackcanada.org", blank: true },
+];
+
+export const navLinks = [
+  { label: "About", href: "aboutSection" },
+  { label: "Venue", href: "locationInfoSection" },
+  { label: "Sponsors", href: "sponsorSection" },
+  { label: "FAQs", href: "faqsSection" },
+  { label: "Team", href: "teamsSection" },
+];
