3bcc3866ba164f4e49080d3ab36a410b2e63d63d
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-02 01:54:16 -0400

    remove location section temporarily

--- src/config/site.ts
+++ src/config/site.ts
@@ -56,25 +56,30 @@ export const aboutSection = {
 };
 
 export const locationSection = {
-  tag: "Yep, it's happening",
-  collegeName: "Laurier University",
-  cards: [
+  tag: "Save the Date",
+  eventName: "Tech Innovators Summit",
+  details: [
     {
-      title: "Event Details",
+      title: "Overview",
       description:
-        "Join us for an exciting event at Laurier University. Don't miss out on this opportunity!",
+        "Be part of the Tech Innovators Summit where developers, designers, and tech enthusiasts come together to create the next big thing.",
+      image: "/placeholder.svg?height=200&width=400",
+    },
+    {
+      title: "Venue",
+      description: "456 Startup Lane, Innovation Park, IP 67890",
+      image: "/placeholder.svg?height=200&width=400",
+    },
+    {
+      title: "Event Dates",
+      description: "November 20-22, 2024<br>10:00 AM - 5:00 PM daily",
       image: null,
     },
     {
-      title: "Location",
-      description: "75 University Ave W, Waterloo, ON N2L 3C5",
-      image: "/api/placeholder/400/200",
-    },
-    {
-      title: "Schedule",
-      description:
-        "Check out our event schedule and plan your day accordingly.",
-      image: null,
+      title: "Map & Directions",
+      description: "Easily find your way to the venue",
+      image:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.215707164939!2d-73.98823492346994!3d40.74844097138798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1686977881642!5m2!1sen!2sus",
     },
   ],
 };
